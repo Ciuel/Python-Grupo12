2d824a1e91fecdd38bd730bb34bbbd85ea67c37d
Lautaro Torchia <Torchia.lautaro2@gmail.com>
2021-04-04 11:19:00 -0300

    Con los conceptos de la clase 4, movi los archivos nombre,eval1,eval2 a otro archivo y los importe en el programa, dejandolo mas prolijo

--- Actividades/Actividad 1.py
+++ Actividades/Actividad 1.py
@@ -1,6 +1,4 @@
-nombres = """'Agustin','Alan','Andrés','Ariadna','Bautista','CAROLINA','CESAR','David','Diego','Dolores','DYLAN','ELIANA','Emanuel','Fabián','Facundo','Facundo','FEDERICO','FEDERICO','GONZALO','Gregorio','Ignacio','Jonathan','Jonathan','Jorge','JOSE','JUAN','Juan','Juan','Julian','Julieta','LAUTARO','Leonel','LUIS','Luis','Marcos','María','MATEO','Matias','Nicolás','NICOLÁS','Noelia','Pablo','Priscila','TOMAS','Tomás','Ulises','Yanina'"""
-eval1 = '''81,60,72,24,15,91,12,70,29,42,16,3,35,67,10,57,11,69,12,77,13,86,48,65,51,41,87,43,10,87,91,15,44,85,73,37,42,95,18,7,74,60,9,65,93,63,74'''
-eval2 = '''30,95,28,84,84,43,66,51,4,11,58,10,13,34,96,71,86,37,64,13,8,87,14,14,49,27,55,69,77,59,57,40,96,24,30,73,95,19,47,15,31,39,15,74,33,57,10'''
+from ArchivoStrings import *
 
 
 def string_a_int(string_in):
@@ -48,10 +46,12 @@ def ordenar(lst_reordenado, criterio_para_reordenar):
     return sorted(lst_reordenado, key=lambda x: x[criterio_para_reordenar - 1])
 
 
+
+
 #Main
-lst_nombres = Modificando_string(nombres)
-lst_eval1 = string_a_int(eval1)
-lst_eval2 = string_a_int(eval2)
+lst_nombres = Modificando_string(nombres) #Nombres ahora se encuentra en el modulo importado
+lst_eval1 = string_a_int(eval1)  #eval1 ahora se encuentra en el modulo importado
+lst_eval2 = string_a_int(eval2)  #eval2 ahora se encuentra en el modulo importado
 lst_alumnos, promedio = calcuar_nota_final(lst_nombres, lst_eval1, lst_eval2)
 
 print("""Los alumnos son:
---
+++ Actividades/ArchivoStrings.py
@@ -0,0 +1,3 @@
+nombres = """'Agustin','Alan','Andrés','Ariadna','Bautista','CAROLINA','CESAR','David','Diego','Dolores','DYLAN','ELIANA','Emanuel','Fabián','Facundo','Facundo','FEDERICO','FEDERICO','GONZALO','Gregorio','Ignacio','Jonathan','Jonathan','Jorge','JOSE','JUAN','Juan','Juan','Julian','Julieta','LAUTARO','Leonel','LUIS','Luis','Marcos','María','MATEO','Matias','Nicolás','NICOLÁS','Noelia','Pablo','Priscila','TOMAS','Tomás','Ulises','Yanina'"""
+eval1 = '''81,60,72,24,15,91,12,70,29,42,16,3,35,67,10,57,11,69,12,77,13,86,48,65,51,41,87,43,10,87,91,15,44,85,73,37,42,95,18,7,74,60,9,65,93,63,74'''
+eval2 = '''30,95,28,84,84,43,66,51,4,11,58,10,13,34,96,71,86,37,64,13,8,87,14,14,49,27,55,69,77,59,57,40,96,24,30,73,95,19,47,15,31,39,15,74,33,57,10'''
