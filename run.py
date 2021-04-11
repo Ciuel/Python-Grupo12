93a59d7f79752c01d0580c43d744b17a3b578865
Ciuel <juantvicens@gmail.com>
2021-04-11 17:29:05 -0300

    Prueba json

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
-import sys
+import json
+archivo = open("bandas.json", "w")
+datos = [
+{"nombre": "William Campbell", "ciudad": "La Plata", "ref": "www.instagram.com/williamcampbellok"},
+{"nombre": "Buendia", "ciudad": "La Plata", "ref":"https://buendia.bandcamp.com/"},
+{"nombre": "Lúmine", "ciudad": "La Plata", "ref": "https://www.instagram.com/luminelp/"}]
+json.dump(datos, archivo,indent=2)
+archivo.close()
 
-print(sys.platform)
-print(sys.path)
-
+archivo = open("bandas.json", "r")
+datos = json.load(archivo)
+datos_a_mostrar = json.dumps(datos, indent=4)
+print(datos_a_mostrar)
+archivo.close()
