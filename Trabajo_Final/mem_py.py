f1846fc40616fc0a1fb0f498c7533dc79b324c3a
Ciuel <juantvicens@gmail.com>
2021-04-23 14:53:00 -0300

    Ventana de scores

--- Trabajo_Final/mem_py.py
+++ Trabajo_Final/mem_py.py
@@ -1,9 +1,13 @@
 #Programa ejecutable
-from src.Components import login,config
+from src.Components import score
+from src.Components import login, config
+
 
 def main():
     login.start()
     config.start()
+    score.start()
+
 
 
 if __name__=="__main__":
