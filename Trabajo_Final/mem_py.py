caf12c36c220b50747795c3d5e4e407b5606ea59
Lautaro Torchia <Torchia.lautaro2@gmail.com>
2021-04-23 15:01:25 -0300

    Cambios nuevos

--- Trabajo_Final/mem_py.py
+++ Trabajo_Final/mem_py.py
@@ -5,13 +5,9 @@ from src.Components import login, config
 
 def main():
     login.start()
-<<<<<<< HEAD
     config.start("3")
-=======
-    config.start()
     score.start()
 
->>>>>>> f1846fc40616fc0a1fb0f498c7533dc79b324c3a
 
 
 if __name__=="__main__":
